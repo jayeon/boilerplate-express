9d89f84ebc87e0dce0a702aa1dd4fc967c74e6d4
jayeon <12667837+jayeon@users.noreply.github.com>
2022-06-09 01:31:24 -0400

    updated

--- myApp.js
+++ myApp.js
@@ -1,32 +1,32 @@
 let express = require('express');
 let app = express();
+let dotenv = require('dotenv').config()
 
 console.log("Hello World")
 
-app.get('/', (req, res) => {
-    res.send("Hello Express");
+app.use('/', (req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
 })
 
+app.get('/', (req, res) => res.sendFile(absolutePath))
 
+app.use('/public', express.static('public'))
 
+app.get('/json', (req, res) => {
+    message = "Hello json"
+    if (process.env.MESSAGE_STYLE == 'uppercase') {
+        message = message.toUpperCase();
+    }
+    res.json({"message": message})
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString()
+    next()
+}, (req, res) => {
+    res.json({"time": req.time})
+})
 
 
 
